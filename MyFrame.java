19bcc991e1df1284d22c5b4dedfa436472bbaa91
MREAL <mreal72@gmail.com>
2019-05-12 10:00:08 +0200

    Modification de la classe MyFrame.java

--- MyFrame.java
+++ MyFrame.java
@@ -1,5 +1,22 @@
 package fr.mreal.MyfirstFrame;
 
-public class MyFrame {
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8424931871184260285L;
+
+	public MyFrame() {
+	
+		super ("Test de fenêtre");
+		this.setSize( 300,200 );
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.setVisible(true);
+		
+	}
+
 
 }
